5645b50bcf051e8d50e605ad85d564fef1a32884
Lawson <abbylaw247@gmail.com>
2023-11-28 18:47:34 -0500

    main reupload
    fixed some stuff

--- main.cpp
+++ main.cpp
@@ -1,34 +1,61 @@
 // Project 3 - Main 
 #include "jaro.cpp" // The Jaro algorithm
-#include <queue>
 #include <string>
-#include <vector>
 using namespace std;
 
+
+string* readFile()
+{
+    // Read in input file (all the passwords) and store into an array (0 = first in txt, and array.size-1 = last)
+    string password;
+    string passwordArray[100000];
+
+    ifstream file("input.txt");
+
+    if (file.is_open())
+    {
+        // Only loop while there are lines (passwords) to be read:
+        for(int i = 0; i < 100000; i++)
+        {
+            getline(file, password);
+            // Add to the array of passwords
+            passwordArray[i] = password;
+        }
+        file.close();
+    }
+
+    for (int i = 0; i < 100000; i++)
+    {
+        cout << password.at(i) << endl;
+    }
+
+
+}
 
 /// NOTE: temporary functions below, would be easier to separate into two classes! Just laying out the ideas here:
 // For both data structures below: output the uniqueness of the input password, and then compare it to passwords in passwordArray (moved to data structure)
 // Output the similar passwords, and only exit output if certain escape key is pressed (e?). Output calculation time until the escape was pressed.
-void redBlackTree(vector<string>passwordArray, string password)
+void redBlackTree(string* passwordArray, string password)
 {
 	cout << "Red - Black Tree" << endl;
 }
-void hashMap(vector<string> passwordArray, string password)
+void hashMap(string* passwordArray, string password)
 {
 	cout << "Hash Map" << endl;
 }
 
 // Menu implementation: User input will be handled within here as well:
-void menu(vector<string> passwordArray)
+void menu(string& passwordArray)
 {
 	int structureChoice;
 	string password;
+    bool exitProgram = false;
 
 	cout << "Quirky Password Checker" << endl;
 	cout << "===========================" << endl;
 
 	// Run until the menu is exited
-	while (!exit)
+	while (!exitProgram)
 	{
 		cout << "Pick your data structure (type 1 or 2):" << endl;
 		cout << "1. Red-Black Tree" << endl; /// Red-black tree is subject to change to a different structure!
@@ -47,14 +74,14 @@ void menu(vector<string> passwordArray)
 		switch (structureChoice)
 		{
 		case 1:
-			redBlackTree(passwordArray, password);
+			redBlackTree(&passwordArray, password);
 			break;
 		case 2:
-			hashMap(passwordArray, password);
+			hashMap(&passwordArray, password);
 			break;
 		default:
 			cout << "Incorrect input, try again." << endl;
-			exit;
+            exitProgram = true;
 		}
 	}
 }
@@ -62,28 +89,9 @@ void menu(vector<string> passwordArray)
 // Main: Reads input.txt, and calls menu (which calls the structures)
 int main()
 {
-	// Read in input file (all the passwords) and store into an array (0 = first in txt, and array.size-1 = last)
-	string password;
-	vector <string> passwordArray;
-
-	ifstream file("input.txt");
-
-	if (file.is_open())
-	{
-		// Only loop while there are lines (passwords) to be read:
-		while (getline(file, password))
-			// Add to the array of passwords
-			passwordArray.push_back(password);
-		file.close();
-	}
-
-	for (int i = 0; i < passwordArray.size(); i++)
-	{
-		cout << password.at(i) << endl;
-	}
-
+    string *passwordArray = readFile();
 	// Now that passwords are stored in temporary array via file, show the menu:
-	menu(passwordArray);
+	menu(*passwordArray);
 
 	return 0;
 }
