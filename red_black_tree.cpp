c2a3c8709b0e69701162c27446d1556467b02ac4
Lawson <abbylaw247@gmail.com>
2023-12-03 19:59:20 -0500

    Update red_black_tree.cpp
    Fixed commenting/whitespace + max limit

--- red_black_tree.cpp
+++ red_black_tree.cpp
@@ -3,31 +3,29 @@
 #include <string>
 // #include <queue>
 using namespace std;
-#define MAX_LIMIT 100
+#define MAX_LIMIT 10
 
+// Red-Black Tree class:
 class RBTree {
+    // Each Node will be a Key/Value Pair, the tree will Search/Store based on the Key
     struct Node {
-        // Each Node will be a Key/Value Pair
-        // The Tree will Search/Store based on the Key
-        // The Key is the Jaro-Value of the Password
-        float jaroVal;
-        // The Value is the given Password
-        string pass;
-        // The original position in the passwords text file
-        int origin;
-        // Red = true, Black = false
-        bool color;
-        // Keep track of Left, Right, and Parent nodes
-        Node *parent, *left, *right;
+        float jaroVal; // The Key is the Jaro-Value of the Password
+        string pass; // The Value is the given Password
+        int origin; // The original position in the passwords text file
+        bool color; // Red = true, Black = false
+        Node *parent, *left, *right; // Keep track of Left, Right, and Parent nodes
+
         // Null node initializer
-        Node() {
+        Node()
+        {
             jaroVal = -1;
             pass = "";
             // Null nodes are always black
             color = false;
             parent = left = right = nullptr;
         }
-        Node(float jaroVal, string pass, int origin) {
+        Node(float jaroVal, string pass, int origin)
+        {
             this->jaroVal = jaroVal;
             this->pass = pass;
             this->origin = origin;
@@ -37,34 +35,26 @@ class RBTree {
         }
     };
     Node* root;
-    // Helper node to ensure invalid addresses aren't accessed in opertations
-    // Attached to nodes with 0/1 children
-    Node* nullNode;
-    // Keeps track of current size of tree
-    unsigned int size = 0;
-    public:
-    // Default Constructor
+    Node* nullNode; // Attached to nodes with 0/1 children: Helps keep track of leaf nodes!
+    unsigned int size = 0; // Keeps track of current size of tree, initialize to 0
+
+public:
     RBTree();
-    // Deconstructor
     ~RBTree();
-    // Deconstructor Helper Function
-    void deleteTree(Node* n);
-    // Main Methods
+    void deleteTree(Node* n); // Deconstructor Helper Function
     // Inserts new node into tree
     void insert(const float& jaroVal, const string& pass, const int& origin);
     // Balances tree after insertion
     void treeUpkeep(Node* n);
-    // Performs left rotate at node
     void leftRotate(Node* n);
-    // Performs right rotate at node
     void rightRotate(Node* n);
     // Returns password and its origin index for quirk score calculations
-    void getTop(string& pass, int& origin) {
+    void getTop(string& pass, int& origin)
+    {
         pass = root->pass;
         origin = root->origin;
     }
-    // Silly Functions
-    // void levelPrint();
+
     // Prints reverse inorder traversal of tree
     void reverseInOrder();
     // Helper function for reverseInOrder()
@@ -72,31 +62,28 @@ class RBTree {
 };
 
 // Default Constructor
-RBTree::RBTree() {
+RBTree::RBTree()
+{
     nullNode = new Node;
-    // Assigns root as a null node
-    root = nullNode;
-    // Initializes size of tree to 0
-    size = 0;
+    root = nullNode; // Assigns root as a null node
+    size = 0; // Initializes size of tree to 0
 }
 
 // Deletes each node in the tree
-RBTree::~RBTree() {
+RBTree::~RBTree()
+{
     deleteTree(root);
 }
 
-// Deletes tree in post order traversal
+// Deletes tree in post order traversal and only delete when size > 0
 void RBTree::deleteTree(Node* n) {
-    // If tree doesn't exist, don't delete
     if(size > 0) {
         // If tree left doesn't exist (or is null node), don't traverse
-        if(n->left != nullNode && n->left) {
+        if(n->left != nullNode && n->left)
             deleteTree(n->left);
-        }
         // If tree right doesn't exist (or is null node), don't traverse
-        if(n->right != nullNode && n->right) {
+        if(n->right != nullNode && n->right)
             deleteTree(n->right);
-        }
         // Decrease size of tree and delete current node
         --size;
         delete n;
@@ -104,59 +91,60 @@ void RBTree::deleteTree(Node* n) {
 }
 
 // Insert a new node into the tree, and upkeep (balance) as needed
-void RBTree::insert(const float& jaroVal, const string& pass, const int& origin) {
-    // Update size of tree
+void RBTree::insert(const float& jaroVal, const string& pass, const int& origin)
+{
     ++size;
-    // Create new leaf node
-    Node* n = new Node(jaroVal, pass, origin);
-    // Assign children as null node
-    n->left = n->right = nullNode;
+    Node* n = new Node(jaroVal, pass, origin); // Create new leaf node
+    n->left = n->right = nullNode; // Since it's a leaf: children are nullNode
+
     // If root doesn't exist, assign n as new root
-    if(root == nullNode) {
-        // Root must be Black
+    if(root == nullNode)
+    {
+        // Root must be Black!
         n->color = false;
         root = n;
         return;
     }
-    // Temp node to navigate our tree and insert n in the correct spot
-    Node* temp = root;
-    // Parent node to follow temp and keep track of n's parent
-    Node* parent;
+
+    Node* temp = root; // Temp node to navigate our tree and insert n in the correct spot
+    Node* parent; // Parent keeps track of the node before any child nodes
+
     // While we haven't reached the end of our tree
-    while(temp != nullNode) {
-        // Parent tracks temp
+    while(temp != nullNode)
+    {
         parent = temp;
-        // If temp's current value is greater than n's
-        // Traverse Left
-        if(n->jaroVal < temp->jaroVal) temp = temp->left;
-        // Else Traverse Right
-        else temp = temp->right;
+        // If temp's current value is greater than n's, traverse Left
+        if(n->jaroVal < temp->jaroVal)
+            temp = temp->left;
+        else
+            temp = temp->right;
     }
     // Once we've reached our destination, update n's parent
     n->parent = parent;
-    // Connect parent node to n
+
     // If n's value is less than its parent's, it's the left child
-    if(n->jaroVal < parent->jaroVal) parent->left = n;
-    // Else, it's the right child
+    if(n->jaroVal < parent->jaroVal)
+        parent->left = n;
     else parent->right = n;
+
     // Current Node needs a grandparent for RBTree Balancing
-    // Tree won't need to be rebalanced with height of 1 or 2
     if(n->parent->parent) treeUpkeep(n);
 }
 
-// Upkeep the tree: (self balancing and recoloring)
-// Starting at inserted node, balance tree, moving up each iteration
-// until the top (root) of the tree is reached.
-void RBTree::treeUpkeep(Node* n) {
+// Upkeep the tree: (self-balancing and recoloring)
+// Starting at inserted node, balance tree, moving up each iteration until the top (root) of the tree is reached.
+void RBTree::treeUpkeep(Node* n)
+{
     Node* unc;
     // Balance only if two consecutive Red nodes
     while (n->parent->color) {
         // Check whether uncle node is to left or right of grandparent
-        if(n->parent == n->parent->parent->right) {
-            // Uncle is left child of grandparent
+        if(n->parent == n->parent->parent->right)
+        {
             unc = n->parent->parent->left;
             // If uncle is red => flip colors
-            if(unc->color) {
+            if(unc->color)
+            {
                 // Flipping colors of uncle, parent, and grandparent nodes
                 n->parent->color = false;
                 unc->color = false;
@@ -166,13 +154,14 @@ void RBTree::treeUpkeep(Node* n) {
             }
             // Else uncle is black => rotate
             else {
-                // if n is the left child of its parent
-                if(n == n->parent->left) {
+                if(n == n->parent->left)
+                {
                     // move pointer to its parent and perform a
                     // right rotation at the node
                     n = n->parent;
                     rightRotate(n);
                 }
+
                 // recolor the parent and grandparent nodes
                 n->parent->color = false;
                 n->parent->parent->color = true;
@@ -180,11 +169,12 @@ void RBTree::treeUpkeep(Node* n) {
                 leftRotate(n->parent->parent);
             }
         }
+        // Do same for the right:
         else {
-            // Uncle is right child of grandparent
             unc = n->parent->parent->right;
             // If uncle is red => flip colors
-            if(unc->color) {
+            if(unc->color)
+            {
                 // Flipping colors of uncle, parent, and grandparent nodes
                 n->parent->color = false;
                 unc->color = false;
@@ -194,21 +184,21 @@ void RBTree::treeUpkeep(Node* n) {
             }
             // Else uncle is black => rotate
             else {
-                // if n is the right child of its parent
-                if(n == n->parent->right) {
+                if(n == n->parent->right)
+                {
                     // move pointer to its parent and perform a
                     // left rotation at the node
                     n = n->parent;
                     leftRotate(n);
                 }
-                // recolor the parent and grandparent nodes
+
+                // Recolor the parent and grandparent nodes
                 n->parent->color = false;
                 n->parent->parent->color = true;
-                // perform a right rotate on n
+                // Perform a right rotate on n
                 rightRotate(n->parent->parent);
             }
         }
-        // If we've reached the top of the tree (root) stop
         if(n == root) break;
     }
     // Ensure root is still colored black
@@ -216,27 +206,28 @@ void RBTree::treeUpkeep(Node* n) {
 }
 
 // Left Rotate:
-void RBTree::leftRotate(Node* n) 
+void RBTree::leftRotate(Node* n)
 {
     // Store right child node of n
     Node* child = n->right;
     // Reassign child node's parent as n's parent (grandparent node)
     child->parent = n->parent;
-    // Find n's parent
     // If nullptr, then reassign root as child
-    if(n->parent == nullptr) root = child;
+    if(n->parent == nullptr)
+        root = child;
     // If n is left child of its parent, parent's left child is now the child node
-    else if(n->parent->left == n) n->parent->left = child;
+    else if(n->parent->left == n)
+        n->parent->left = child;
     // If n is right child of its parent, parent's right child is now the child node
     else n->parent->right = child;
     // n's parent is now child
     n->parent = child;
-    // if child's left subtree was empty, do nothing
-    // if it wasn't empty, the left subtree's parent is now n
-    if(child->left != nullNode) child->left->parent = n;
+    // If it wasn't empty, the left subtree's parent is now n
+    if(child->left != nullNode)
+        child->left->parent = n;
     // n's right subtree is now child's left
     n->right = child->left;
-    // finally, child's left subtree is now n
+    // Finally, child's left subtree is now n
     child->left = n;
 }
 
@@ -247,62 +238,45 @@ void RBTree::rightRotate(Node* n)
     Node* child = n->left;
     // Reassign child node's parent as n's parent (grandparent node)
     child->parent = n->parent;
-    // Find n's parent
-    // If nullptr, then reassign root as child
-    if(n->parent == nullptr) root = child;
+    if(n->parent == nullptr)
+        root = child;
     // If n is right child of its parent, parent's right child is swapped
-    else if(n->parent->right == n) n->parent->right = child;
-    // If n is left child of its parent, parent's left child is swapped
-    else n->parent->left = child;
+    else if(n->parent->right == n)
+        n->parent->right = child;
+    // Otherwise swap left and child
+    else
+        n->parent->left = child;
     // n's parent is now child
     n->parent = child;
-    // if child's right subtree was empty, do nothing
-    // if it wasn't empty, the right subtree's parent is now n
-    if(child->right != nullNode) child->right->parent = n;
+    // If it wasn't empty, the right subtree's parent is now n
+    if(child->right != nullNode)
+        child->right->parent = n;
     // n's left subtree is now child's right
     n->left = child->right;
-    // finally, child's right subtree is now n
+    // Finally, child's right subtree is now n
     child->right = n;
 }
 
-/*
-// Print level order of tree:
-void RBTree::levelPrint() {
-    queue<Node*> q;
-    Node* temp;
-    q.push(root);
-    while(!q.empty()) {
-        temp = q.front();
-        q.pop();
-        cout << temp->pass << " ";
-        if(temp->left != nullNode) 
-            q.push(temp->left);
-        if(temp->right != nullNode) 
-            q.push(temp->right);
-    }
-    cout << endl;
-}
-*/
-
 // Print In Order Traversal of tree:
-void RBTree::reverseInOrder() {
+void RBTree::reverseInOrder()
+{
     // Initializing values to be passed into helper print function
     int count = 0, limit = MAX_LIMIT;
     reverseInOrderHelper(root, count, limit);
-    cout << endl;
 }
-// Helper function for inOrder Print
-// Print from the largest to smallest jaro values
-void RBTree::reverseInOrderHelper(Node* n, int& count, int& limit) {
-    // If right child is not a null node (empty tree), traverse right
-    // Only traverse the number of nodes we need to (limit)
-    if(n->right != nullNode && count <= limit) reverseInOrderHelper(n->right, count, limit);
-    // Print nodes current count, password, and jaro value
-    // Only print as many as specified by limit
-    if(++count <= limit) cout << count << ": " << n->pass << ", " << n->jaroVal << ", origin: " << n->origin << "\n";
-    // If left child is not a null node (empty tree), traverse left
-    // Only traverse the number of nodes we need to (limit)
-    if(n->left != nullNode && count <= limit) reverseInOrderHelper(n->left, count, limit);
+
+// Helper function for inOrder Print: print from the largest to smallest jaro values
+void RBTree::reverseInOrderHelper(Node* n, int& count, int& limit)
+{
+    // If right child is not a null node (empty tree), traverse right: traverse the number of nodes we need to (limit)
+    if(n->right != nullNode && count <= limit)
+        reverseInOrderHelper(n->right, count, limit);
+    // Print nodes current count, password, and jaro value: print as many as specified by limit
+    if(++count <= limit)
+        cout << count << ": " << n->pass << ", " << n->jaroVal << "\n";
+    // If left child is not a null node (empty tree), traverse left: traverse the number of nodes we need to (limit)
+    if(n->left != nullNode && count <= limit)
+        reverseInOrderHelper(n->left, count, limit);
 }
 
 /*
