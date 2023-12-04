bcd4e2b053c2f795c8b622b545be1767e9b7025b
Lawson <abbylaw247@gmail.com>
2023-12-04 15:44:15 -0500

    Update main.cpp
    I goofed up and ctrl v'd the wrong file! Here is the real one!

--- main.cpp
+++ main.cpp
@@ -1,155 +1,219 @@
+// =========================== Project 3 - Quirky Password Checker ===========================
+// Aidan Doherty, David Willis, Abigail Lawson | Fall 2023
+
+#include "jaro.cpp" // The Jaro algorithm
+#include "red_black_tree.cpp" // The Red-Black Tree
+#include "heap.cpp" // The Max Heap
 #include <string>
-#include <iostream>
-#include <algorithm>
-#include <unordered_set>
-#include <fstream>
-#include <stdexcept>
-#include <vector>
-#include <cctype>
+#include <ctime>
+#define MAX_SIZE 100000
 using namespace std;
 
-class stringComparison {
-public:
-    int numTranspositions = 0, numMatches = 0;
-    string headString, secondaryString;
-    stringComparison(string headString, string secondaryString);
-    float jaroDist();
-    float quirkDist();
+// Read in the 100k passwords via the file (input.txt) and database: REF: https://lucidar.me/en/security/list-of-100000-most-common-passwords/ 
+string* readFile(string filename)
+{
+    // Read in input file (all the passwords) and store into an array (0 = first in txt, and array.size-1 = last)
+    string password;
+    string* passwordArray = new string[100000];
+    ifstream file(filename);
 
-};
-
-double scoreLength(string password);
-double scoreJaro(string password, string secondary);
-float scoreSet(string password);
-double scorePosition(int position);
-float jaroDistance(string s1, string s2);
-float getQuirkScore(string password, string mostSimilar, int mostSimilarPosition);
-
-float getQuirkScore(string password, string mostSimilar, int mostSimilarPosition) {
-    double positionScore = scorePosition(mostSimilarPosition);
-    int setScore = scoreSet(password);
-    stringComparison passCompare(password, mostSimilar);
-    float jaroScore = scoreJaro(password, mostSimilar);
-    double lengthScore = scoreLength(password);
-    double quirkScore = 0;
-    quirkScore += setScore;
-    quirkScore += positionScore;
-    quirkScore += jaroScore;
-    quirkScore += lengthScore;
-    if (scoreJaro(password, mostSimilar) >= 1)
-        quirkScore /= 100;
-    return 100 / 19 * quirkScore;
+    if (file.is_open())
+    {
+        // Only loop while there are lines (passwords) to be read:
+        for(int i = 0; i < 100000; i++)
+        {
+            getline(file, password);
+            // Add to the array of passwords
+            passwordArray[i] = password;
+        }
+        file.close();
+    }
+    else
+        cout << "File was not opened" << endl;
+    
+    return passwordArray;
 }
 
-double scoreLength(string password) {
-    return password.length() / 4 - 1;
+// For both data structures below: output the uniqueness of the input password, and then compare it to passwords in passwordArray (moved to data structure)
+// Output the similar passwords, and only exit output if certain escape key is pressed (e?). Output calculation time until the escape was pressed.
+void redBlackTree(string* passwordArray, string password)
+{
+    cout << "Red - Black Tree" << endl;
+    cout << "---------------------------" << endl;
+    
+    clock_t t_redBlack; // Time of red-black tree
+    clock_t t_maxHeap; // Time of max heap
+    int topindex = 0; // topIndex keeps track fo the index of the top Jaro value, in terms of the input.txt file's lines
+    t_redBlack = clock();
+    RBTree myTree;
+    
+    // Ensure that we only insert up to max size (100,000) defined at top of main!
+    for(int i = 0; i < MAX_SIZE; i++)
+        myTree.insert(jaroDistance(password, passwordArray[i]), passwordArray[i], ++topindex);
+    
+    t_redBlack = clock() - t_redBlack;
+    float cr_redBlack = (float)t_redBlack/CLOCKS_PER_SEC; // Click rate of red-black tree
+    cout << "Time it took: " << endl;
+    cout << "Red-Black: " << t_redBlack << " clicks at " << CLOCKS_PER_SEC << " cps (" << cr_redBlack << " seconds)" << endl;
+
+    // Repeat for the max heap (to show the time difference)
+    t_maxHeap = clock();
+    Heap myHeap;
+    
+    for(int i = 0; i < MAX_SIZE; i++)
+        myHeap.insert(jaroDistance(password, passwordArray[i]), passwordArray[i], i);
+    
+    t_maxHeap = clock() - t_maxHeap;
+    float cr_maxHeap = (float)t_maxHeap/CLOCKS_PER_SEC; // Click rate of max heap
+    cout << "Max Heap: " << t_maxHeap << " clicks at " << CLOCKS_PER_SEC << " cps (" << cr_maxHeap << " seconds)" << endl;
+
+    // Print the comparison of them!
+    if(t_redBlack > t_maxHeap)
+        cout << "The Max Heap ran faster!" << endl;
+    else
+        cout << "The Red-Black tree ran faster!" << endl;
+    cout << "---------------------------" << endl;
+
+    // Calculate the quirky score:
+    string mostSimilar;
+    int mostSimilarPos;
+    myTree.getTop(mostSimilar, mostSimilarPos);
+
+    float quirkScore = getQuirkScore(password, mostSimilar, mostSimilarPos);
+    cout << "Quirky Score: " << quirkScore << endl;
+    if(quirkScore < 25)
+        cout << "Blech, your password is not quirky at all!" << endl;
+    else if(quirkScore < 50)
+        cout << "Uhh your password is kinda quirky, I guess... Could be better..." << endl;
+    else if (quirkScore < 75)
+        cout << "Wow uhm, I really like how quirky and unique your password is *blush*" << endl;
+    else
+        cout << "WOWZA!!! What a quirky Password! Where'd you get a password like that???" << endl;
+    cout << "===========================" << endl;
+    //printf ("It took me %d clicks (%f seconds).\n",t,((float)t)/CLOCKS_PER_SEC);
+    myTree.reverseInOrder();
 }
 
-double scoreJaro(string password, string s2) {
-    stringComparison compare(password, s2);
-    return 4 * (1 - compare.quirkDist());
+// Max Heap:
+void maxHeap(string* passwordArray, string password)
+{
+    // Get the time of the max heap: (in clicks and cps)
+    cout << "Max Heap" << endl;
+    cout << "---------------------------" << endl;
+
+    clock_t t_maxHeap; // Time of max heap
+    clock_t t_redBlack; // Time of red-black tree
+    t_maxHeap = clock();
+    Heap myHeap;
+    
+    // Ensure that we only insert up to max size (100,000) defined at top of main!
+    for(int i = 0; i < MAX_SIZE; i++)
+        myHeap.insert(jaroDistance(password, passwordArray[i]), passwordArray[i], i);
+    
+    t_maxHeap = clock() - t_maxHeap;
+    float cr_maxHeap = (float)t_maxHeap/CLOCKS_PER_SEC; // Click rate of max heap
+    cout << "Time it took: " << endl;
+    cout << "Max Heap: " << t_maxHeap << " clicks at " << CLOCKS_PER_SEC << " cps (" << cr_maxHeap << " seconds)" << endl;
+
+    // Repeat for the red-black tree (to show the time difference)
+    t_redBlack = clock();
+    RBTree myTree;
+    
+    for(int i = 0; i < MAX_SIZE; i++)
+        myTree.insert(jaroDistance(password, passwordArray[i]), passwordArray[i], 0);
+    
+    t_redBlack = clock() - t_redBlack;
+    float cr_redBlack = (float)t_redBlack/CLOCKS_PER_SEC; // Click rate of red-black tree
+    cout << "Red-Black Tree: " << t_redBlack << " clicks at " << CLOCKS_PER_SEC << " cps (" << cr_redBlack << " seconds)" << endl;
+
+    // Print the comparison of them!
+    if(cr_redBlack > cr_maxHeap)
+        cout << "The Max Heap ran faster!" << endl;
+    else
+        cout << "The Red-Black tree ran faster!" << endl;
+    cout << "---------------------------" << endl;
+
+    // Calculate the quirky score:
+    float quirkScore = getQuirkScore(password, myHeap.getTopPassword(), myHeap.getTopIndex());
+    cout << "Quirky Score: " << quirkScore << endl;
+    if(quirkScore < 25)
+        cout << "Blech, your password is not quirky at all!" << endl;
+    else if(quirkScore < 50)
+        cout << "Uhh your password is kinda quirky, I guess... Could be better..." << endl;
+    else if (quirkScore < 75)
+        cout << "Wow uhm, I really like how quirky and unique your password is *blush*" << endl;
+    else
+        cout << "WOWZA!!!! What a quirky Password! Where'd you get a password like that???" << endl;
+    cout << "===========================" << endl;
+
+    // Print out the actual heap: (print top 10)
+    myHeap.printTop();
 }
 
-stringComparison::stringComparison(string headString, string secondaryString) {
-    this->headString = headString;
-    this->secondaryString = secondaryString;
-    int passwordLength = headString.length();
-    // m is the number of matching characters
-    // t is the number of transpositions
-    // searchableDistance is the distance within which a character is considered matching
-    int searchableDistance;
-    // s1 will always be longer than s2
-    string s1 = headString, s2 = secondaryString;
-    if (headString.length() < secondaryString.length())
-        swap(s1, s2);
-    searchableDistance = (s1.length() / 2) - 1;
-    // Any characters counted in a match can only be counted once, so we keep track of them in a set
-    unordered_set<int> matchedIndices;
-    int greatestIndex = min(s1.length(), s2.length() + searchableDistance);
-    // This is the execution of the algorithm
-    for(int i = 0; i < greatestIndex; i++) {
-        // Test to see if there is a match
-        // Find the first unique match
-        for(int j = max(0, i - searchableDistance); j < min(int(s2.length()), i + searchableDistance + 1); j++) {
-            if(s1[i] == s2[j] && matchedIndices.find(j) == matchedIndices.end()) {
-                // If it is not an exact match, its both a match and a transposition
-                if(i != j)
-                    numTranspositions++;
-                numMatches++;
-                matchedIndices.insert(j);
+// Menu implementation: User input will be handled within here as well:
+void menu(string* passwordArray)
+{
+    int structureChoice;
+    int exitOption;
+    string password;
+    bool exitProgram = false;
+
+    cout << "Quirky Password Checker" << endl;
+    cout << "===========================" << endl;
+
+    // Run until the menu is exited
+    while (!exitProgram)
+    {
+        cout << "Pick your data structure (type 1 or 2):" << endl;
+        cout << "1. Red-Black Tree" << endl; 
+        cout << "2. Max Heap" << endl;
+        cout << "Type any other key to exit program." << endl;
+        cin >> structureChoice;
+
+        // Check if need to exit:
+        if (structureChoice != 1 && structureChoice != 2)
+        {
+            cout << "Exiting program.";
+            exitProgram = true;
+        }
+        // Otherwise prompt for password:
+        cout << "Input your password: ";
+        cin >> password;
+        cout << endl;
+
+        switch (structureChoice)
+        {
+            case 1:
+                redBlackTree(passwordArray, password);
                 break;
-            }
+            case 2:
+                maxHeap(passwordArray, password);
+                break;
+            default:
+                cout << "Incorrect input, try again." << endl;
+                exitProgram = true;
+        }
+
+        cout << "===========================" << endl;
+        cout << "Would you like to continue, or exit the program?" << endl;
+        cout << "1. Continue" << endl;
+        cout << "Type any other key to exit program." << endl;
+        cin >> exitOption;
+
+        if(exitOption != 1)
+        {
+            cout << "Exiting program.";
+            exitProgram = true;
         }
     }
 }
 
-// Calculates the jaroDistance between two strings:
-float stringComparison::jaroDist() {
-    // Calculate and return the Jaro Distance
-    float jaro = 0;
-    if (numMatches != 0) {
-        jaro = float(numMatches) / headString.length();
-        jaro += float(numMatches) / secondaryString.length();
-        jaro += float(numMatches - numTranspositions / 2.0) / numMatches;
-        jaro /= 3;
-    }
-    return jaro;
-}
+// Main: Reads input.txt, and calls menu (which calls the structures)
+int main()
+{
+    string* passwordArray = readFile("input.txt");
+    // Now that passwords are stored in a temporary array via file, show the menu/prompt input
+    menu(passwordArray);
 
-// Calculates the quirk distance, which goes into the quirk score calculations
-float stringComparison::quirkDist() {
-    // Calculate and return the Quirky distance
-    float dist = 0;
-    if(numMatches != 0) {
-        dist = 3 * float(numMatches) / headString.length();
-        dist += float(numMatches - numTranspositions) / numMatches;
-        dist /= 4;
-    }
-    return dist;
-}
-
-// Set the score of the password based on uppercase, special characters, numbers, and lowercase:
-float scoreSet(string password) {
-    int setScore = 0, numLower = 0, numUpper = 0, numSpecial = 0, numNumber = 0;
-    for(char c : password) {
-        if(islower(c))
-            numLower++;
-        else if(isupper(c))
-            numUpper++;
-        else if(isdigit(c))
-            numNumber++;
-        else
-            numSpecial++;
-    }
-    if (numUpper > 0)
-        setScore++;
-    if(numNumber > 0)
-        setScore++;
-    if(numSpecial > 0)
-        setScore += 2;
-    int leastOccurringSet = min(numLower, numUpper);
-    leastOccurringSet = min(leastOccurringSet, numSpecial);
-    leastOccurringSet = min(leastOccurringSet, numNumber);
-    float variance = float(leastOccurringSet) / password.length();
-    if (variance >= 0.05)
-        setScore++;
-    if(variance >= 0.1)
-        setScore++;
-    return float(setScore) * 3/2;
-}
-
-// Adds points to the score based off of the position in the original file! (lower position in file will add to score)
-double scorePosition(int position) {
-    int score = 0;
-    if(position > 25000)
-        score += 1;
-    if(position > 50000)
-        score += 1;
-    return score;
-}
-
-// s1 is the user password, s2 is the password you're comparing it to
-float jaroDistance(string s1, string s2) {
-    stringComparison comparison(s1, s2);
-    return comparison.jaroDist();
+    return 0;
 }
